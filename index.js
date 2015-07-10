7a63b369a39e50790b46feec89d0dc4ec3a7467b
vagrant <vagrant@lighthouse-labs>
2015-07-09 21:25:11 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+var app = require('express')();
+var express = require('express');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.use('/samples', express.static(__dirname + '/samples'));
+
+
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('sound', function(){
+    io.emit('sound');
+
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('kick', function(){
+    io.emit('kick');
+
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('keypress', function(){
+    io.emit('keypress');
+
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('keypress2', function(){
+    io.emit('keypress2');
+
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+
+
+
+
+
+
