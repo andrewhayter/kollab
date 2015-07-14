ed3ab7f4f328ded53617b06a897cac28dcb45591
Andrew Hayter <andrewhayter@gmail.com>
2015-07-14 12:26:52 -0700

    NexusUI basic sequencing

--- public/seq.js
+++ public/seq.js
@@ -1,14 +1,10 @@
-var Dilla = require('dilla');
+/* jshint quotmark: false, browser: true, jquery: true, devel: true */
+/* globals nx, pattern, synthkeys, toggle1, dial1 */
+"use strict";
 var Context = window.AudioContext || window.webkitAudioContext;
 var audioContext = new Context();
 
-var bpmTempo = 240;
-
-var dilla = new Dilla(audioContext, {
-  'tempo': bpmTempo,
-  'beatsPerBar': 4,
-  'loopLength': 2
-});
+var bpmTempo = 120;
 
 // store sounds for decoded sound buffers
 
@@ -16,7 +12,8 @@ var sounds = {};
 
 // load found file and decode data
 
-function loadSound(name, done) {
+function loadSound(name, done)
+{
   var request = new XMLHttpRequest();
   request.open('GET', 'dilla/' + name + '.wav', true);
   request.responseType = 'arraybuffer';
@@ -35,90 +32,80 @@ var soundNames = [
 'kick', 'snare', 'hihat'
 ];
 
-// get names of sounds to load from soundName Array
-
-function loadNextSound() {
-  var soundName = soundNames.shift();
-  if(soundName) {
-    loadSound(soundName, loadNextSound);
-  }
-}
-
-loadNextSound();
-
-// Sequence rows
-
-
-dilla.set('snare', [
-  ['1.1.91'],
-  ['1.3.91'],
-  ['2.1.91'],
-  ['2.4.03']
-  ]);
-
-// dilla.set('hihat', [
-//   ['*.1.01', { 'gain': 0.4 }],
-//   ['*.2.01', { 'gain': 0.5 }],
-//   ['*.3.01', { 'gain': 0.6 }],
-//   ['*.4.01', { 'gain': 0.5 }],
-//   ['*.4.53', { 'gain': 0.6 }]
-//   ]);
-
-
-dilla.on('step', function onStep(step) {
-  source = audioContext.createBufferSource(); // create buffer source
-  source.buffer = sounds[step.id]; // set buffer to sound object called from set
-  var gainNode = source.gainNode = audioContext.createGain(); // create gainNode
-  var gainVolume = step.args.gain || 1; // if gainVolume not set, use 1
-  source.connect(gainNode); // output source to gainNode
-  gainNode.connect(audioContext.destination); // output gainNode to speakers
-  gainNode.gain.value = gainVolume; // set gainNode value to gainVolume
-  source.start(step.time); // start step seq
-});
-
-
 // Nexus UI Stuff
 
-nx.onload = function(){
-  nx.sendsTo("js")
+function soundsLoaded()
+{
+  nx.sendsTo("js");
   nx.colorize("#60B3C8");
 
   pattern.col = 16;
-  pattern.row = 1;
+  pattern.row = Object.keys(sounds).length;
   pattern.resize($("#content").width(), 250);
   pattern.draw();
   pattern.init();
 
-      // seqplay.col = 16;
-      // seqplay.row = 1;
-      // seqplay.resize($("#content").width(), 250);
-      // seqplay.draw();
-      // seqplay.init();
+  synthkeys.keypattern = ['w','b','w','b','w','w','b','w','b','w','b','w'];
+  synthkeys.resize($("#synth").width(), 250);
+  synthkeys.draw();
+  synthkeys.init();
 
-      synthkeys.keypattern = ['w','b','w','b','w','w','b','w','b','w','b','w'];
-      synthkeys.resize($("#synth").width(), 250);
-      synthkeys.draw();
-      synthkeys.init();
+  function play(buffer)
+  {
+    var source = audioContext.createBufferSource();
+    source.buffer = buffer;
+    source.connect(audioContext.destination);
+    source.start(); // start step seq
+  }
 
-      pattern.on('*', function(data) {
-        // var kickSeq = [];
-        // kickSeq.push([data.col]);
-        // // console.log(data.value)
-        // dilla.set('kick', kickSeq);
-        console.log(data)
-      });
-
-      toggle1.on('*', function(data) {
-        if (data.value == 1) {
-          dilla.start();
-          pattern.sequence;
-        } else {
-          pattern.stop();
-          dilla.stop();
-        }
-      });
-
-      dial1.on('value', function(data) {
-        console.log('d1', data);
+  pattern.on('*', function(data)
+  {
+    var soundNames = Object.keys(sounds);
+    if(data.list)
+    {
+      //Sequencer event
+      data.list.map(function(state, idx)
+      {
+        if(!state) { return; }
+        var sound = sounds[soundNames[idx]];
+        play(sound);
       });
     }
+    else
+    {
+      //Click event
+    }
+  });
+
+  toggle1.on('*', function(data)
+  {
+    if (data.value == 1) {
+      pattern.sequence(bpmTempo);
+    } else {
+      pattern.stop();
+    }
+  });
+
+  dial1.on('value', function(data)
+  {
+    console.log('d1', data);
+  });
+}
+
+function loadNextSound()
+{
+  var soundName = soundNames.shift();
+  if(soundName) {
+    loadSound(soundName, loadNextSound);
+  }
+  else
+  {
+    soundsLoaded();
+  }
+}
+
+nx.onload = function()
+{
+  loadNextSound();
+};
+
