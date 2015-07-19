0205bf0734c2f2c1e08cb58f76015e18b070ec79
robin <vagrant@lighthouse-labs>
2015-07-19 14:59:10 -0700

    username started

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
+var users = {};
+var userCount = 0;
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -16,7 +18,9 @@ app.use('/public', express.static(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //messages
+//keeping with socket as opposed to client for user
 io.on('connection', function(socket){
+  var addedUser = false;
   console.log('a user connected');
 
   socket.on('disconnect', function(){
@@ -28,6 +32,19 @@ io.on('connection', function(socket){
     console.log(msg);
     console.log("hello");
   });
+
+  socket.on("join", function(name){
+      people[socket.id] = name;
+      socket.emit("update", "You have connected to the server.");
+      socket.sockets.emit("update", name + " has joined the server.")
+      console.log(name);
+      socket.sockets.emit("update-people", people);
+  });
+
+
+
+
+
 
 // Receving empty object
   socket.on('play note', function(event){
